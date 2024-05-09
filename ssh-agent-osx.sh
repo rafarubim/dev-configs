c2b61718ea276ac7c788f98e468a7c0b10c300f9
Rafael Rubim Cabral <rafael.rubim.cabral@gmail.com>
2024-05-08 21:48:47 -0300

    Update and rename ssh-agent-linux.sh to ssh-agent-osx.sh

--- ssh-agent-osx.sh
+++ ssh-agent-osx.sh
@@ -2,7 +2,7 @@
 
 trap "echo > /dev/null" INT
 eval "$(ssh-agent -s)" > /dev/null
-if ! ssh-add ~/.ssh/id_ed25519; then
+if ! ssh-add --apple-use-keychain ~/.ssh/id_ed25519; then
   eval "$(ssh-agent -k)" > /dev/null
   echo -e "\nFailed to add key to agent.\nThe agent was killed. \o/  ¬_o"
 fi
